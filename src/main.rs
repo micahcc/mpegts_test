89df98aebfb7eed602d9b1c979b388e3a1b9b12f
Micah Chambers (eos) <mchambers@anduril.com>
2024-03-21 00:08:55 -0700

    more work

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,115 @@
 use clap::Parser;
 use log::error;
 use log::info;
+use mpeg2ts::es::StreamId;
+use mpeg2ts::pes::PesHeader;
+use mpeg2ts::time::Timestamp;
+use mpeg2ts::ts::{
+    payload, AdaptationExtensionField, AdaptationField, ContinuityCounter, Pid, ReadTsPacket,
+    TransportScramblingControl, TsHeader, TsPacket, TsPacketReader, TsPacketWriter, TsPayload,
+    WriteTsPacket,
+};
 use std::fs::File;
 use std::io::prelude::*;
 
 mod image;
 use image::{generate_image, PixFmt};
+
+struct TsStreamer<W> {
+    writer: TsPacketWriter<W>,
+    counter: ContinuityCounter,
+
+    input_frame_number: u64,
+    input_frame_unix_micros: u64,
+    frame_rate: f64,
+    stream_id: StreamId,
+
+    output_frame_number: u64,
+}
+
+impl<W: std::io::Write> TsStreamer<W> {
+    pub fn set_next_frame_meta(&mut self, frame_number: u64, unix_timestamp_micros: u64) {
+        self.input_frame_number = frame_number;
+        self.input_frame_unix_micros = unix_timestamp_micros;
+    }
+}
+
+impl<W: std::io::Write> TsStreamer<W> {
+    pub fn new_stream(stream: W) -> anyhow::Result<Self> {
+        let mut writer = TsPacketWriter::<W>::new(stream);
+        let stream_id = StreamId::new_video(0)?;
+        Ok(Self {
+            writer,
+            counter: ContinuityCounter::new(),
+            input_frame_number: 0,
+            input_frame_unix_micros: 0,
+            output_frame_number: 0,
+            stream_id,
+            frame_rate: 30.0,
+        })
+    }
+}
+
+impl<W: std::io::Write> std::io::Write for TsStreamer<W> {
+    fn write(&mut self, data: &[u8]) -> Result<usize, std::io::Error> {
+        // write a PES packet
+
+        // make a packet
+        // copy data into packet
+        info!(
+            "Input count: {}, Output Frame Count: {}",
+            self.input_frame_number, self.output_frame_number
+        );
+
+        let pts = 0; // todo
+        let data = [0; 5];
+        let packet = TsPacket {
+            header: TsHeader {
+                transport_error_indicator: false,
+                transport_priority: false,
+                pid: Pid::new(32).expect("allocate pid"),
+                continuity_counter: ContinuityCounter::from_u8(self.counter.as_u8())
+                    .expect("counter"),
+                transport_scrambling_control: TransportScramblingControl::NotScrambled,
+            },
+            adaptation_field: Some(AdaptationField {
+                discontinuity_indicator: false,
+                random_access_indicator: true,
+                es_priority_indicator: false,
+                pcr: None,
+                opcr: None,
+                splice_countdown: None,
+                transport_private_data: vec![],
+                extension: None,
+            }),
+            payload: Some(TsPayload::Pes(payload::Pes {
+                header: PesHeader {
+                    copyright: false,
+                    data_alignment_indicator: true,
+                    escr: None,
+                    original_or_copy: true,
+                    stream_id: self.stream_id,
+                    priority: false,
+                    dts: None,
+                    pts: Some(Timestamp::new(pts).expect("make timestamp")),
+                },
+                pes_packet_len: 0,
+                data: payload::Bytes::new(&data).expect("make bytes"),
+            })),
+        };
+
+        self.writer.write_ts_packet(&packet).expect("write packet");
+        self.counter.increment();
+        return Ok(0);
+    }
+
+    fn flush(&mut self) -> Result<(), std::io::Error> {
+        // not clear if we need to pass the flush all the way down
+        // that would require adding a flush method or mut_stream
+        // to TsPacketWriter
+        return Ok(());
+    }
+}
 
 /// Simple program to greet a person
 #[derive(Parser, Debug)]
@@ -14,6 +118,10 @@ struct Args {
     /// Where to send, can be file:///path udp://ip:port or - for stdout
     #[arg(short, long)]
     target: String,
+
+    /// Send a mpeg-ts stream instead of a raw x264 stream.
+    #[arg(long)]
+    mpegts: bool,
 
     /// Number of frames to produce.
     #[arg(short, long)]
@@ -28,7 +136,26 @@ struct Args {
     ysize: u32,
 }
 
-pub fn send_to_file(
+pub fn send_mpegts_to_file(
+    num_frames: u32,
+    width: u32,
+    height: u32,
+    filename: &str,
+) -> anyhow::Result<()> {
+    let mut fd = File::create(filename)?;
+    let mut ts_streamer = TsStreamer::new_stream(fd)?;
+    let mut my_h264_writer = less_avc::H264Writer::new(ts_streamer).unwrap();
+
+    for i in 0..num_frames {
+        let input_yuv = generate_image(i as u32, &PixFmt::Rgb8, width, height).unwrap();
+        let frame_view = input_yuv.view();
+        my_h264_writer.write(&frame_view).unwrap();
+    }
+
+    return Ok(());
+}
+
+pub fn send_x264_to_file(
     num_frames: u32,
     width: u32,
     height: u32,
@@ -60,7 +187,11 @@ fn main() -> anyhow::Result<()> {
 
     match args.target {
         t if t.starts_with("file://") => {
-            send_to_file(args.num_frames, args.xsize, args.ysize, &t[7..])?;
+            if args.mpegts {
+                send_mpegts_to_file(args.num_frames, args.xsize, args.ysize, &t[7..])?;
+            } else {
+                send_x264_to_file(args.num_frames, args.xsize, args.ysize, &t[7..])?;
+            }
         }
         t if t.starts_with("udp://") => {
             let t = &t[6..];
@@ -76,7 +207,11 @@ fn main() -> anyhow::Result<()> {
             send_to_stdout(args.num_frames)?;
         }
         t => {
-            send_to_file(args.num_frames, args.xsize, args.ysize, &t)?;
+            if args.mpegts {
+                send_mpegts_to_file(args.num_frames, args.xsize, args.ysize, &t)?;
+            } else {
+                send_x264_to_file(args.num_frames, args.xsize, args.ysize, &t)?;
+            }
         }
     }
 
